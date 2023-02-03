b3d3d87a6687825c509553a831eb389d451afb51
swgj <1803057261@qq.com>
2023-02-03 16:46:37 +0800

    分组，并增加了树相关

--- array/三数之和.js
+++ array/三数之和.js
@@ -4,9 +4,9 @@ function threesum(nums) {
     if (nums == null || len < 3) return ans;
     nums.sort((a, b) => a - b); // 排序
     for (let i = 0; i < len; i++) {
-        if (nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
+        if (nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大�?0，所以结束循�?
         if (i > 0 && nums[i] == nums[i - 1]) continue; // 去重
-        let L = i + 1;//取i之后的值
+        let L = i + 1;//取i之后的�?
         let R = len - 1;
         while (L < R) {
             const sum = nums[i] + nums[L] + nums[R];
---
+++ tree/二叉树着色.js
@@ -0,0 +1,71 @@
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+var buildTree = function (nums) {
+    var root = new TreeNode(nums[0]);
+    var queue = [];
+    queue.push(root);
+    var cur;
+    var lineNodeNum = 2;
+    var startIndex = 1;
+    var restLength = nums.length - 1;
+    while (restLength > 0) {
+        for (var i = startIndex; i < startIndex + lineNodeNum; i = i + 2) {
+            if (i == nums.length) return root;
+            cur = queue.shift();
+            if (nums[i] != null) {
+                cur.left = new TreeNode(nums[i]);
+                queue.push(cur.left);
+            }
+
+            if (i + 1 == nums.length) return root;
+            if (nums[i + 1] != null) {
+                cur.right = new TreeNode(nums[i + 1]);
+                queue.push(cur.right);
+            }
+        }
+        startIndex += lineNodeNum;
+        restLength -= lineNodeNum;
+        lineNodeNum = queue.length * 2;
+    }
+    return root;
+}
+
+let tree = buildTree([5, 9, 6, null, null, 7, null, null, 8, 1, 4, null, null, null, 3, null, 2])
+
+var btreeGameWinningMove = function (root, n, x) {
+    let xNode;
+    const find = (node, x) => {
+        if (xNode || !node) {
+            return;
+        }
+        if (node.val === x) {
+            xNode = node;
+            return;
+        }
+        find(node.left, x);
+        find(node.right, x);
+    }
+
+    const getSubtreeSize = (node) => {
+        if (!node) {
+            return 0;
+        }
+        return 1 + getSubtreeSize(node.left) + getSubtreeSize(node.right);
+    };
+    find(root, x);
+    const leftSize = getSubtreeSize(xNode.left);
+    if (leftSize >= Math.floor((n + 1) / 2)) {
+        return true;
+    }
+    const rightSize = getSubtreeSize(xNode.right);
+    if (rightSize >= Math.floor((n + 1) / 2)) {
+        return true;
+    }
+    const remain = n - 1 - leftSize - rightSize;
+    return remain >= Math.floor((n + 1) / 2);
+}
+console.log(btreeGameWinningMove(tree, 9, 8));
