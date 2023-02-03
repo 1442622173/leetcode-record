00602808c8cafdec252f8cafa17096fbfc6ff859
kuzing <1442622173@qq.com>
2023-02-04 00:09:58 +0800

    解决了一些乱码，更新了二叉树着色

--- tree/二叉树着色.js
+++ tree/二叉树着色.js
@@ -33,58 +33,30 @@ var buildTree = function (nums) {
     }
     return root;
 }
-
-let tree = buildTree([5, 9, 6, null, null, 7, null, null, 8, 1, 4, null, null, null, 3, null, 2])
-
-var btreeGameWinningMove = function (root, n, x) {
-    let xNode;
-    const find = (node, x) => {
-        if (xNode || !node) {
-            return;
-        }
-        if (node.val === x) {
-            xNode = node;
-            return;
-        }
-        find(node.left, x);
-        find(node.right, x);
-    }
-
-    const getSubtreeSize = (node) => {
-        if (!node) {
-            return 0;
-        }
-        return 1 + getSubtreeSize(node.left) + getSubtreeSize(node.right);
-    };
-    find(root, x);
-    const leftSize = getSubtreeSize(xNode.left);
-    if (leftSize >= Math.floor((n + 1) / 2)) {
-        return true;
-    }
-    const rightSize = getSubtreeSize(xNode.right);
-    if (rightSize >= Math.floor((n + 1) / 2)) {
-        return true;
-    }
-    const remain = n - 1 - leftSize - rightSize;
-    return remain >= Math.floor((n + 1) / 2);
-}
-console.log(btreeGameWinningMove(tree, 9, 8));
-//ÎÒµÄ´úÂë
+//ä»¥ä¸‹ä¸ºç®—æ³•
 var btreeGameWinningMove = function (root, n, x) {
     function nodesum(root) {
         if (!root) return 0
         return 1 + nodesum(root.left) + nodesum(root.right)
     }
+    let node
     function findnode(root, x) {
         if (!root) return
-        if (root.val == x) return root
+        if (root.val == x) {
+            node = root
+            return 
+        }
         findnode(root.left, x)
         findnode(root.right, x)
     }
-    let node = findnode(root, x)
+    findnode(root, x)
     let sumr = nodesum(node.right)
     let suml = nodesum(node.left)
     let other = n - 1 - sumr - suml
-    let max = Math.max
+    let max = Math.max(suml, sumr, other)
     return max > n - max
 };
+
+let tree = buildTree([5, 9, 6, null, null, 7, null, null, 8, 1, 4, null, null, null, 3, null, 2])
+
+console.log(btreeGameWinningMove(tree, 9, 8));
