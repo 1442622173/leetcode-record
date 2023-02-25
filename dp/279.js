dd27f22c9eccf7eb04e25483e8b43cf0dfdb8e6d
kuzing <1442622173@qq.com>
2023-02-25 19:59:58 +0800

    279

--- dp/279.js
+++ dp/279.js
@@ -10,3 +10,34 @@ var numSquares = function (n) {
 };
 
 console.log(numSquares(12));
+//数学方法
+var numSquares02 = function (n) {
+    if (isPerfectSquare(n)) {
+        return 1;
+    }
+    if (checkAnswer4(n)) {
+        return 4;
+    }
+    for (let i = 1; i * i <= n; i++) {
+        let j = n - i * i;
+        if (isPerfectSquare(j)) {
+            return 2;
+        }
+    }
+    return 3;
+}
+
+// 判断是否为完全平方数
+const isPerfectSquare = (x) => {
+    const y = Math.floor(Math.sqrt(x));
+    return y * y == x;
+}
+
+// 判断是否能表示为 4^k*(8m+7)
+const checkAnswer4 = (x) => {
+    while (x % 4 == 0) {
+        x /= 4;
+    }
+    return x % 8 == 7;
+}
+console.log(numSquares02(12));
